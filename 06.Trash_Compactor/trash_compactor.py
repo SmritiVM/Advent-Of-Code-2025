2381728fc1ff32aaa8cee86af15ccab1a5d49743
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2025-12-06 14:18:18 +0530

    part 2, but tacky

--- 06.Trash_Compactor/trash_compactor.py
+++ 06.Trash_Compactor/trash_compactor.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import defaultdict
 
 def parse_input(file):
     # N number of lines
@@ -12,22 +13,54 @@ def parse_input(file):
             operands.append(list(map(int, line.strip().split())))
     return operands, operators
 
+def parse_input_2(file):
+    operands = defaultdict(list)
+    operator_positions = defaultdict(str)
+    for line in file:
+        for index, char in enumerate(line):
+            if char.isdigit():
+                operands[index].append(char)
+            if char in ['+', '*']:
+                operator_positions[index] = char
+    max_index = 0
+    for key in operands:
+        operands[key] = int(''.join(operands[key]))
+        max_index = max(max_index, key)
+    operator_positions[max_index + 2] = None 
+    return operands, operator_positions
+    
+
 def operator_function(operator: str):
     if operator == '+': 
         return lambda x, y: x + y
     if operator == '*':
         return lambda x, y: x * y
+    return lambda x: x
 
-def solve_worksheet():
+def solve_worksheet(operands, operators):
     total = 0
-    for index, operand in enumerate(OPERANDS[0]):
+    for index, operand in enumerate(operands[0]):
         problem = [operand]
-        for operands in OPERANDS[1:]:
+        for operands in operands[1:]:
             problem.append(operands[index])
-        total += reduce(operator_function(OPERATORS[index]), problem)
+        total += reduce(operator_function(operators[index]), problem)
     return total
 
+def solve_worksheet_r_l(operands, operators):
+    total = 0
+    prev_position = 0
+    for position in operators:
+        if position == 0: continue
+        problem = list()
+        for x in range(prev_position, position - 1):
+            problem.append(operands[x])
+        total += reduce(operator_function(operators[prev_position]), problem)
+        prev_position = position
+    return total
+
+
 with open ('./input.txt') as file:
-    OPERANDS, OPERATORS = parse_input(file)
-    # print(OPERANDS, OPERATORS)
-    print(solve_worksheet())
+    # operands, operators = parse_input(file)
+    # print(solve_worksheet(operands, operators))
+    operands, operators = parse_input_2(file)
+    print(solve_worksheet_r_l(operands, operators))
