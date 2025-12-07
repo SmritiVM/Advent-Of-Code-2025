15c4aa575cc80fca7f0ed6d9c75f7790cc79ded3
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2025-12-07 14:58:38 +0530

    part 2

--- 05.Cafeteria/cafeteria.py
+++ 05.Cafeteria/cafeteria.py
@@ -21,6 +21,32 @@ def count_fresh():
 
     return count
 
+
+# Find total non overlapping distinct subsets
+def merge_intervals(intervals):
+    if not intervals: return []
+
+    intervals.sort()
+    merged = [intervals[0]]
+
+    for current in intervals[1:]:
+        prev_start, prev_end = merged[-1]
+        curr_start, curr_end = current
+        if curr_start <= prev_end: # overlapping subsets
+            merged[-1][1] = max(prev_end, curr_end) # prev interval is extended
+        else:
+            merged.append(current)
+    return merged
+
+def total_possible_fresh():
+    total = 0
+    for start, end in MERGED_FRESH_INGREDIENTS:
+        total += (end - start + 1)
+    return total
+
 with open ('./input.txt') as file:
     FRESH_INGREDIENTS, INGREDIENTS = parse_input(file)
     print(count_fresh())
+    MERGED_FRESH_INGREDIENTS = merge_intervals(FRESH_INGREDIENTS)
+    # print(MERGED_FRESH_INGREDIENTS)
+    print(total_possible_fresh())
